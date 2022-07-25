72b5cad918872214ae5c1a896d2e7b7421ba4378
Kontosoros <mkontosoros@gmail.com>
2022-07-25 15:43:53 +0300

    ai models

--- AI_MODELS/training_process/classifier_diabete_prediction.py
+++ AI_MODELS/training_process/classifier_diabete_prediction.py
@@ -7,10 +7,10 @@ df  = pd.read_csv("diabetes.csv")
 X = df[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
        'BMI', 'DiabetesPedigreeFunction', 'Age']]
 y = df['Outcome']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X.to_numpy(), y.to_numpy(), test_size=0.20, random_state=42)
 model = svm.SVC()
 model.fit(X_train,y_train)
 filename = 'diabetes_classifier_model.sav'
-#pickle.dump(model, open(filename, 'wb'))
+pickle.dump(model, open(filename, 'wb'))
 accuracy = model.score(X_test,y_test)
 print("ACCURACY :",accuracy)
--- AI_MODELS/training_process/classifier_heart_attack_prediction.py
+++ AI_MODELS/training_process/classifier_heart_attack_prediction.py
@@ -9,7 +9,7 @@ X = df[['age', 'sex',  'trtbps', 'chol', 'fbs',  'thalachh',
        'exng',    'thall',]]
 
 y = df['output']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X.to_numpy(), y.to_numpy(), test_size=0.20, random_state=42)
 model = svm.SVC(kernel='linear')
 model.fit(X_train,y_train)
 filename = 'heart_attack_classifier_model.sav'
